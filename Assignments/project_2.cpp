84073801e4fa51229d18a139e32c604486ad03ff
caTr1x <75480109+catr1xLiu@users.noreply.github.com>
2025-09-25 14:55:55 -0400

    fixed a bug in project 2

--- Assignments/project_2.cpp
+++ Assignments/project_2.cpp
@@ -1,22 +1,22 @@
 #include<iostream>
 #include<cassert>
 
-void line(int n, int i)
+void line(unsigned int n, unsigned int i)
 {
-  for (int j = 0; j < n-i; j++)
+  for (unsigned int j = 0; j < n-i; j++)
     std::cout << " ";
-  for (int j = 0; j < 1 + i*2; j++)
+  for (unsigned int j = 0; j < 1 + i*2; j++)
     std::cout << "*";
   std::cout << std::endl;
 }
 
 void pattern(unsigned int n)
 {
-  for (int i = n; i > 0; i--)
+  for (unsigned i = n; i > 0; i--)
   {
     line(n, i);
   }
-  for (int i = 0; i <= n; i++)
+  for (unsigned int i = 0; i <= n; i++)
   {
     line(n, i);
   }
@@ -43,7 +43,7 @@ unsigned int log10(unsigned int n)
   for (unsigned int m = 0; m < 10; m++)
     if (exp10(m) > n)
       return m-1;
-  return 11;
+  return 9;
 }
 
 unsigned int count(unsigned int n, unsigned int bit)
@@ -64,39 +64,27 @@ unsigned int swap_bytes(unsigned int n, unsigned int b0, unsigned int b1)
 {
   assert(0 <= b0 && b0 <= 3 && 0 <= b1 && b1 <= 3);
 
-  unsigned int bytes[4];
-  for (unsigned int i = 0; i < 4; i++)
-  {
-    bytes[i] = n % (16*16);
-    n /= (16*16);
-  }
+  unsigned int num_b0{ n%exp256(b0+1)/exp256(b0) }, num_b1{ n%exp256(b1+1)/exp256(b1) };
   
-  unsigned int tmp = bytes[b0];
-  bytes[b0] = bytes[b1];
-  bytes[b1] = tmp;
+  std::cout << std::hex << num_b0 << " " << std::hex << num_b1 << std::endl;
+  n -= num_b0 * exp256(b0) + num_b1 * exp256(b1);
+  n += num_b1 * exp256(b0) + num_b0 * exp256(b1);
 
-  unsigned int result = 0;
-  for (int i = 0; i < 4; i++)
-  {
-    std::cout << bytes[i] << std::endl;
-    result += bytes[i] * exp256(i);
-  }
-  return result;
+  return n;
 }
 
-int main() 
-{
-  for (int i = 0; i < 5; i++)
-  {
-    pattern(i);
-    std::cout<<std::endl<<std::endl;
-  }
-  std::cout << log10(100) << std::endl;
-  std::cout << log10(1200) << std::endl;
+// int main() 
+// {
+//   for (int i = 0; i < 5; i++)
+//   {
+//     pattern(i);
+//     std::cout<<std::endl<<std::endl;
+//   }
+//   std::cout << log10(101U) << std::endl;
+//   std::cout << log10(147483649U) << std::endl;
 
-  std::cout << count(0x03020100U, 1) << std::endl;
-  std::cout <<count(0x03020100U, 0) << std::endl;
+//   std::cout << count(0x03020100U, 1) << std::endl;
+//   std::cout << count(0x03020100U, 0) << std::endl;
 
-  std::cout << swap_bytes(0x03020100U, 1, 3) << std::endl;
-  std::cout << 0x01020300U << std::endl;
-}
+//   std::cout << swap_bytes(0x6f1b5a04U, 1, 3) << std::endl;
+// }
