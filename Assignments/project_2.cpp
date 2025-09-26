0ad20eacca32b35c9b77eb2a13bc39704be4f87f
caTr1xLiu <lyr20070621@gmail.com>
2025-09-26 15:15:21 -0400

    Fixed code for project 2

--- Assignments/project_2.cpp
+++ Assignments/project_2.cpp
@@ -30,14 +30,6 @@ unsigned int exp10(unsigned int m)
   return result;
 }
 
-unsigned int exp256(unsigned int exp)
-{
-  unsigned int result = 1;
-  for (unsigned int i = 0; i < exp; i++)
-    result *= 256;
-  return result; 
-}
-
 unsigned int log10(unsigned int n)
 {
   for (unsigned int m = 0; m < 10; m++)
@@ -60,31 +52,46 @@ unsigned int count(unsigned int n, unsigned int bit)
   return bit == 1 ? ones_count : 32-ones_count;
 }
 
+unsigned int shift_right(unsigned int n, unsigned int byte)
+{
+  return n >> (byte * 8);
+}
+
+unsigned int shift_left(unsigned int n, unsigned int byte)
+{
+  return n << (byte * 8);
+
+}
+
 unsigned int swap_bytes(unsigned int n, unsigned int b0, unsigned int b1)
 {
   assert(0 <= b0 && b0 <= 3 && 0 <= b1 && b1 <= 3);
 
-  unsigned int num_b0{ n%exp256(b0+1)/exp256(b0) }, num_b1{ n%exp256(b1+1)/exp256(b1) };
-  
+  unsigned int num_b0{ shift_right(shift_left(n, 3-b0), 3) };
+  unsigned int num_b1{ shift_right(shift_left(n, 3-b1), 3) };
+
   std::cout << std::hex << num_b0 << " " << std::hex << num_b1 << std::endl;
-  n -= num_b0 * exp256(b0) + num_b1 * exp256(b1);
-  n += num_b1 * exp256(b0) + num_b0 * exp256(b1);
+
+  n -= shift_left(num_b0, b0) + shift_left(num_b1, b1);
+  n += shift_left(num_b0, b1) + shift_left(num_b1, b0);
 
   return n;
 }
 
-// int main() 
-// {
-//   for (int i = 0; i < 5; i++)
-//   {
-//     pattern(i);
-//     std::cout<<std::endl<<std::endl;
-//   }
-//   std::cout << log10(101U) << std::endl;
-//   std::cout << log10(147483649U) << std::endl;
+int main_() 
+{
+  for (int i = 0; i < 5; i++)
+  {
+    pattern(i);
+    std::cout<<std::endl<<std::endl;
+  }
+  std::cout << log10(101U) << std::endl;
+  std::cout << log10(147483649U) << std::endl;
 
-//   std::cout << count(0x03020100U, 1) << std::endl;
-//   std::cout << count(0x03020100U, 0) << std::endl;
+  std::cout << count(0x03020100U, 1) << std::endl;
+  std::cout << count(0x03020100U, 0) << std::endl;
 
-//   std::cout << swap_bytes(0x6f1b5a04U, 1, 3) << std::endl;
-// }
+  std::cout << std::hex << swap_bytes(0x6f1b5a04U, 1, 3) << std::endl;
+
+  return 0;
+}
